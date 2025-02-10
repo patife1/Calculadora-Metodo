d81cc9a011a900c22ed4bb51bbc2a6c962fcef4e
Izabelly Márcia <santosizabelly6@gmail.com>
2025-02-10 10:11:41 -0300

    atualizaçao

---
+++ Ola.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualBasic;
+
+namespace Ola
+{
+    class Cumprimentar
+    {
+        public static void ImprimirOla(){
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double num1, num2, result;
+        string operation;
+
+        Console.WriteLine("Calculadora");
+
+        Console.Write("Digite o primeiro número: ");
+        num1 = Convert.ToDouble(Console.ReadLine());
+
+      
+        Console.Write("Digite a operação (+, -, *, /): ");
+        operation = Console.ReadLine();
+
+       
+        Console.Write("Digite o segundo número: ");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+       
+        if (operation == "+")
+        {
+            result = num1 + num2;
+            Console.WriteLine($"Resultado: {num1} + {num2} = {result}");
+        }
+        else if (operation == "-")
+        {
+            result = num1 - num2;
+            Console.WriteLine($"Resultado: {num1} - {num2} = {result}");
+        }
+        else if (operation == "*")
+        {
+            result = num1 * num2;
+            Console.WriteLine($"Resultado: {num1} * {num2} = {result}");
+        }
+        else if (operation == "/")
+        {
+            if (num2 != 0)
+            {
+                result = num1 / num2;
+                Console.WriteLine($"Resultado: {num1} / {num2} = {result}");
+            }
+            else
+            {
+                Console.WriteLine("Erro! Divisão por zero.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Operação inválida!");
+        }
+    }
+}
